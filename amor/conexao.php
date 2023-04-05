7a3be30bf9e2ca548135dd2d5b6cacb31db0ef75
Kaiobrunoreis <kaio.reis1@icloud.com>
2023-04-05 11:10:11 -0300

    atualizacao

---
+++ amor/conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+abstract class ClassConexao{
+public function Conectar()
+{
+    try{
+        $Con=new PDO("mysql:host=kaiosites.mysql.uhserver.com;dbname=kaiosites","kaioreis","159357xatM@");
+        return $Con;
+    }catch (PDOException $Erro){
+        return $Erro->getMessage();
+    }
+}
+}
+?>
